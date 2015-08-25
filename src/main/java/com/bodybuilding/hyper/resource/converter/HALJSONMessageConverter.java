d1207a84a8b08e951b0cb25235781875dc5f5337
chris damour <chris.damour@bodybuilding.com>
2015-08-24 18:18:04 -0600

    comapi-3179 initial stab

---
+++ src/main/java/com/bodybuilding/hyper/resource/converter/HALJSONMessageConverter.java
@@ -0,0 +1,28 @@
+package com.bodybuilding.hyper.resource.converter;
+
+import com.bodybuilding.hyper.resource.HyperResource;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+
+/**
+A MessageConverter used to serialize Hyper Resources as HAL+JSON
+ */
+public class HALJSONMessageConverter extends WriteOnlyHyperResourceMessageConverter {
+    public HALJSONMessageConverter(){
+        super(new MediaType("application", "hal+json"));
+    }
+
+
+
+
+
+    @Override
+    protected void writeInternal(HyperResource resource, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
+
+        String result = "\"This is hal+json representation\"\n" + resource.toString();
+        httpOutputMessage.getBody().write(result.getBytes());
+    }
+}
---
+++ src/main/java/com/bodybuilding/hyper/resource/converter/HTMLMessageConverter.java
@@ -0,0 +1,25 @@
+package com.bodybuilding.hyper.resource.converter;
+
+import com.bodybuilding.hyper.resource.HyperResource;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+
+/**
+ A MessageConverter used to serialize Hyper Resources as HAL+JSON
+ */
+public class HTMLMessageConverter extends WriteOnlyHyperResourceMessageConverter {
+    public HTMLMessageConverter(){
+        super(new MediaType("text", "html"));
+    }
+
+
+    @Override
+    protected void writeInternal(HyperResource resource, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
+
+        String result = "\"This is html representation\"\n" + resource.toString();
+        httpOutputMessage.getBody().write(result.getBytes());
+    }
+}
