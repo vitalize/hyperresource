ce9dc5b5fde60ae577ef3260ef32ecd76c4836de
chris damour <chris.damour@bodybuilding.com>
2015-08-25 13:47:19 -0600

    comapi-3179 test

--- src/main/java/com/bodybuilding/hyper/resource/converters/WriteOnlyHyperResourceMessageConverter.java
+++ src/main/java/com/bodybuilding/hyper/resource/converters/WriteOnlyHyperResourceMessageConverter.java
@@ -1,12 +1,10 @@
-package com.bodybuilding.hyper.resource.converter;
+package com.bodybuilding.hyper.resource.converters;
 
 import com.bodybuilding.hyper.resource.HyperResource;
 import org.springframework.http.HttpInputMessage;
-import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 
 import java.io.IOException;
 
