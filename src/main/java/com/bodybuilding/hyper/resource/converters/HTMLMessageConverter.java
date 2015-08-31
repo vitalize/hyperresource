e6df233ea1bb7cbda10b20b41c63ab9eeacc6a4b
mw100 <mike.walters@bodybuilding.com>
2015-08-31 13:42:30 -0600

    basic example of using mustache templates to serialize HyperResources in the HTMLMessageConverter.

--- src/main/java/com/bodybuilding/hyper/resource/converters/HTMLMessageConverter.java
+++ src/main/java/com/bodybuilding/hyper/resource/converters/HTMLMessageConverter.java
@@ -1,25 +1,46 @@
 package com.bodybuilding.hyper.resource.converters;
 
-import com.bodybuilding.hyper.resource.HyperResource;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
-import java.io.IOException;
+import com.bodybuilding.hyper.resource.HyperResource;
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import com.github.mustachejava.MustacheResolver;
+import com.github.mustachejava.resolver.DefaultResolver;
 
 /**
  * A MessageConverter used to serialize Hyper Resources as HAL+JSON
  */
 public class HTMLMessageConverter extends WriteOnlyHyperResourceMessageConverter {
+	
+	private MustacheFactory mustacheFactory;
+	
     public HTMLMessageConverter() {
         super(new MediaType("text", "html"));
+        setMustacheFactory();
     }
-
-
+    
+    private void setMustacheFactory() {
+    	MustacheResolver mustacheResolver = new DefaultResolver("templates/mustache/");
+		this.mustacheFactory = new DefaultMustacheFactory(mustacheResolver);    	
+    }
+    
     @Override
     protected void writeInternal(HyperResource resource, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
 
-        String result = "This is html representation\n" + resource.toString();
-        httpOutputMessage.getBody().write(result.getBytes());
+    	String templateName = resource.getClass().getSimpleName();
+    	Mustache mustache = mustacheFactory.compile(templateName);
+    	Writer writer = new OutputStreamWriter(httpOutputMessage.getBody());
+    	mustache.execute(writer, resource);
+    	writer.flush();
+    	
     }
+    
 }
