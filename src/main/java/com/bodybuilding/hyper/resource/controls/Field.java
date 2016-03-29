96caa5bfd7c51e06b979806a173a6f10cf3593f6
Mike Walters <mike.walters@bodybuilding.com>
2016-03-29 17:09:45 -0600

    COMAPI-4583: PR feedback: Use inheritance hierarchy to specify Field type.

--- src/main/java/com/bodybuilding/hyper/resource/controls/Field.java
+++ src/main/java/com/bodybuilding/hyper/resource/controls/Field.java
@@ -5,60 +5,22 @@ import lombok.ToString;
 
 @EqualsAndHashCode
 @ToString
-public class Field<T> {
+public abstract class Field<T> {
     
-    private final FieldType type;
     private final String name;
     private final T value;
     
-    private Field(Builder<T> builder) {
-        this.type = builder.type;
-        this.name = builder.name;
-        this.value = builder.value;
+    protected Field(String name, T value) {
+        this.name = name;
+        this.value = value;
     }
     
-    public FieldType getType() {
-        return type;
-    }
-
     public String getName() {
         return name;
     }
-
+    
     public T getValue() {
         return value;
-    }
-    
-    public static enum FieldType {
-        HIDDEN,
-        ;
-    }
-
-    public static class Builder<T> {
-        
-        private FieldType type;
-        private String name;
-        private T value;
-        
-        public Field<T> build() {
-            return new Field<T>(this);
-        }
-        
-        public Builder<T> type(FieldType type) {
-            this.type = type;
-            return this;
-        }
-        
-        public Builder<T> name(String name) {
-            this.name = name;
-            return this;
-        }
-        
-        public Builder<T> value(T value) {
-            this.value = value;
-            return this;
-        }
-        
     }
 
 }
