2b556ff94f2300cc8ef139d50a5b2eb573cacff5
Dmytro Pavlikovskiy <pdaviti@gmail.com>
2016-05-23 12:00:29 -0600

    COMAPI-4967: added back local wrapper templates + returns default StringTemplateSource if template not found

--- src/main/java/com/bodybuilding/hyper/resource/converters/RemoteTemplateLoader.java
+++ src/main/java/com/bodybuilding/hyper/resource/converters/RemoteTemplateLoader.java
@@ -5,12 +5,24 @@ import static org.apache.commons.lang3.Validate.notEmpty;
 import java.io.IOException;
 import java.net.URL;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ResourceLoader;
 
+import com.github.jknack.handlebars.io.StringTemplateSource;
 import com.github.jknack.handlebars.io.TemplateSource;
 import com.github.jknack.handlebars.springmvc.SpringTemplateLoader;
 
+/**
+ * 1) Trying to load template remotely for all templates with //;
+ * 2) Trying to load locally;
+ * 3) Returns default template;
+ */
 public class RemoteTemplateLoader extends SpringTemplateLoader {	
+	
+    private static final Logger LOG = LoggerFactory.getLogger(RemoteTemplateLoader.class);
+
+	private static final StringTemplateSource DEFAULT_SOURCE = new StringTemplateSource("/default", "");
 	
 	public RemoteTemplateLoader(ResourceLoader loader) {
 		super(loader);
@@ -27,7 +39,14 @@ public class RemoteTemplateLoader extends SpringTemplateLoader {
 				return source;
 			}
 		}
-		return super.sourceAt(uri);
+		try{
+			TemplateSource source =  super.sourceAt(uri);
+			return source;
+		} catch (IOException ex){
+			LOG.error("Template {} not found using default source.", uri);
+		}
+		return DEFAULT_SOURCE;
+
 	}
 
 }
