70241042a8d7c9b58dd2b3f40dec12a384868212
Chris DaMour <drdamour@gmail.com>
2016-06-21 01:04:26 -0600

    did a little research, it seems we shouldn't close the output so i will not for now

--- src/main/java/com/bodybuilding/hyper/resource/serializer/html/handlebars/HTMLHandlebarsSerializer.java
+++ src/main/java/com/bodybuilding/hyper/resource/serializer/html/handlebars/HTMLHandlebarsSerializer.java
@@ -44,15 +44,21 @@ public class HTMLHandlebarsSerializer implements HyperResourceSerializer {
         String templateName = resource.getClass().getSimpleName();
         Template template = handlebars.compile(templateName);
 
-        OutputStreamWriter writer = null;
-        try {
-            writer = new OutputStreamWriter(output);
-            template.apply(resource, writer);
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
+        OutputStreamWriter writer = new OutputStreamWriter(output);
+        template.apply(resource, writer);
+
+        //TODO: is this necessary or even a good idea? OutputStreamWriter
+        //uses a string encoder that seems like it would want a flush
+        writer.flush();
+
+        //Not closing as it seems we should not close the passed in outputstream
+        //for example spring wants to flush the output stream see https://github.com/spring-projects/spring-framework/blob/56db1af11dbe51c88c753421e022bc5389361c04/spring-web/src/main/java/org/springframework/http/converter/AbstractGenericHttpMessageConverter.java#L101
+        //after a call to write..so assume that above layers will actually close it
+        //though some converters do close on success see https://github.com/spring-projects/spring-framework/blob/56db1af11dbe51c88c753421e022bc5389361c04/spring-web/src/main/java/org/springframework/http/converter/json/GsonHttpMessageConverter.java#L203
+        //the StringConverter flushes but doesn't close https://github.com/spring-projects/spring-framework/blob/56db1af11dbe51c88c753421e022bc5389361c04/spring-web/src/main/java/org/springframework/http/converter/StringHttpMessageConverter.java#L107 which calls https://github.com/spring-projects/spring-framework/blob/56db1af11dbe51c88c753421e022bc5389361c04/spring-core/src/main/java/org/springframework/util/StreamUtils.java#L110
+
+        //asked at http://stackoverflow.com/questions/37937583/do-i-need-to-flush-or-close-the-outputstream-in-my-custom-spring-web-messageconv
+
     }
     
 }
