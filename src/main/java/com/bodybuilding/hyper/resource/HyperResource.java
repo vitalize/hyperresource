0b30a324252440844c656b8b6b4ad839845a973f
Chris DaMour <drdamour@gmail.com>
2017-07-05 14:37:23 -0600

    move to org.hyperfit.hyperresource naming. setup basic project stuff.

--- src/main/java/com/bodybuilding/hyper/resource/HyperResource.java
+++
@@ -1,8 +0,0 @@
-package com.bodybuilding.hyper.resource;
-
-/**
- * Identifies that a class instance can be interpreted as a HyperResource
- */
-public interface HyperResource {
-
-}
--- src/test/java/com/bodybuilding/hyper/resource/spring/controllers/NoTemplateHyperResource.java
+++
@@ -1,7 +0,0 @@
-package com.bodybuilding.hyper.resource.spring.controllers;
-
-import com.bodybuilding.hyper.resource.HyperResource;
-
-public class NoTemplateHyperResource implements HyperResource {
-
-}
